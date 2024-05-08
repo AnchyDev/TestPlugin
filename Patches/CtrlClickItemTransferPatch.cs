7b81f66548fe976ce84eb33917a64b5ce2409c22
AnchyDev <lewienato@gmail.com>
2024-05-08 23:32:46 +1000

    Added control clicking items to fast transfer items.

---
+++ Patches/CtrlClickItemTransferPatch.cs
@@ -0,0 +1,37 @@
+﻿using BepInEx.Logging;
+
+using HarmonyLib;
+
+using System.Linq;
+
+namespace TestPlugin.Patches;
+
+[HarmonyPatch(typeof(InventoryScreenInput))]
+[HarmonyPatch("OnItem2dClick")]
+internal class CtrlClickItemTransferPatch
+{
+    private static void Postfix(ref tk2dUIItem tk2ditem)
+    {
+        var logger = (ManualLogSource)Logger.Sources.First(l => l.SourceName == PluginInfo.PLUGIN_NAME);
+
+        if (UnityEngine.Input.GetKey(UnityEngine.KeyCode.LeftControl))
+        {
+            logger.LogInfo("Control clicked item");
+
+            var item = tk2ditem as InventoryItem2dInput;
+            if(item is null)
+            {
+                return;
+            }
+
+            if(item.InVicinity)
+            {
+                GameManager.PlayerInventory.AddItem(item.InventoryItem);
+            }
+            else
+            {
+                GameManager.PlayerInventory.Remove(item.InventoryItem);
+            }
+        }
+    }
+}

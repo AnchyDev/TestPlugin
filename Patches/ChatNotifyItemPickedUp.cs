c5cc31eac483d6847539b4b57c36120fbd36ab5e
AnchyDev <lewienato@gmail.com>
2024-05-08 22:47:16 +1000

    Items are printed into chat when looting items.

---
+++ Patches/ChatNotifyItemPickedUp.cs
@@ -0,0 +1,36 @@
+﻿using HarmonyLib;
+
+namespace TestPlugin.Patches;
+
+[HarmonyPatch(typeof(PlayerInventory))]
+[HarmonyPatch("AddItem")]
+internal class ChatNotifyItemPickedUp
+{
+    private static void Postfix(ref InventoryItem item)
+    {
+        if(item is null)
+        {
+            return;
+        }
+
+        GameManager.ChatScreenInput.AddText($"Picked up item ", UnityEngine.Color.white, false);
+        GameManager.ChatScreenInput.AddText(item.ItemDisplayName, GetColorFromItemRarity(item.Rarity));
+    }
+
+    private static UnityEngine.Color GetColorFromItemRarity(InventoryItem.RarityType rarity)
+    {
+        switch(rarity)
+        {
+            case InventoryItem.RarityType.Common:
+                return UnityEngine.Color.white;
+
+            case InventoryItem.RarityType.Uncommon:
+                return UnityEngine.Color.cyan;
+
+            case InventoryItem.RarityType.Rare:
+                return UnityEngine.Color.magenta;
+        }
+
+        return UnityEngine.Color.white;
+    }
+}
